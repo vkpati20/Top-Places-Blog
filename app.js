1d3a0772cd6859aaf3d0ed7fbfef4382f582be4c
Veerendranath Korrapati <vkpati20@gmail.com>
2020-08-06 13:26:50 -0500

    Created a database and added first place

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express"),
       bodyParser = require("body-parser"),
-      mongoose = require("mongoose"),
+      mongoose = require("mongoose")
 
-      Place = require("./models/places");
+//Requiring models for Database
+const Place = require("./models/places");
 
 
 app = express();
@@ -16,13 +17,33 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname+"/public")); //For stylesheets
 
-// mongoose.connect('mongodb://localhost:27017/places_blog_v1', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// })
-//     .then(()=>console.log("Connected to DB"))
-//     .catch(error=>console.log("Error connecting to DB: " + error.message));
+mongoose.connect('mongodb://localhost:27017/places_blog_v1', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+    .then(()=>console.log("Connected to DB"))
+    .catch(error=>console.log("Error connecting to DB: " + error.message));
 
+
+
+
+Place.create(
+    {
+        name: "Veeru",
+        location: "Plano",
+        displayImage: "https://image.png",
+        description: "This is description"
+    },
+    (err, place)=>{
+        if(err)
+            console.log("Error creating place: " + err);
+        else{
+            console.log("Created a new place");
+            console.log(place);
+        }
+
+    }
+)
 
 app.use(indexRoutes);
 app.use("/places", placeRoutes); //Since all placeRoutes start with /places, I'm sending the '/places' prefix to all the places routes
--- models/places.js
+++ models/places.js
@@ -14,20 +14,20 @@ var placeSchema = new mongoose.Schema({
     name: String,
     location: String,
     displayImage: String,
-    description: String,
-    author: {
-        id:{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User"
-        },
-        username: String
-    },
-    comments: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Comment"
-        }
-    ]
+    description: String
+    // author: {
+    //     id:{
+    //         type: mongoose.Schema.Types.ObjectId,
+    //         ref: "User"
+    //     },
+    //     username: String
+    // },
+    // comments: [
+    //     {
+    //         type: mongoose.Schema.Types.ObjectId,
+    //         ref: "Comment"
+    //     }
+    // ]
 })
 
 
